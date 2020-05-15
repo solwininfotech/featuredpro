4db9995b8076529a86b1c2217d0e7cef006dfbe9
Mayur Dhudasia <mmdhudasia@gmail.com>
2020-05-15 17:44:23 +0530

    initial commit

---
+++ registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Solwin Infotech
+ * Solwin Featured Product Extension
+ *
+ * @category   Solwin
+ * @package    Solwin_FeaturedPro
+ * @copyright  Copyright © 2006-2016 Solwin (https://www.solwininfotech.com)
+ * @license    https://www.solwininfotech.com/magento-extension-license/
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Solwin_FeaturedPro',
+    __DIR__
+);
